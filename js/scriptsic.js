46b066f1b0e57a4cc93aa219dbf426086733a583
Jean Jia and Marie Dillard <marie@>
2018-05-29 13:18:05 -0700

    Add toggles for all three flavors.

--- js/scriptsic.js
+++ js/scriptsic.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".clickable1").click(function() {
     $("#vanilla-showing").toggle();
     $("#vanilla-hidden").toggle();
+  });
+  $(".clickable2").click(function() {
+    $("#chocolate-showing").toggle();
+    $("#chocolate-hidden").toggle();
+  });
+  $(".clickable3").click(function() {
+    $("#strawberry-showing").toggle();
+    $("#strawberry-hidden").toggle();
   });
 });
